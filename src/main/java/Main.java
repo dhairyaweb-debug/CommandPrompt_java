746bc9cc540cc69ecb23e01658d36fbb5180544c
Dhairya <dhairyaweb@gmail.com>
2026-06-20 15:24:07 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,7 @@
 import java.util.Scanner;
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws Exception {
@@ -17,17 +19,32 @@ public class Main {
                 continue;
             }
 
-            String[] inputParts = input.split(" ");
+            List<String> inputPartsList = parseArguments(input);
+            if (inputPartsList.isEmpty()) {
+                continue;
+            }
+
+            String[] inputParts = inputPartsList.toArray(new String[0]);
             String command = inputParts[0];
 
             if (command.equals("exit")) {
                 break;
             } else if (command.equals("echo")) {
-                System.out.println(input.substring(5));
+                StringBuilder echoOutput = new StringBuilder();
+                for (int i = 1; i < inputParts.length; i++) {
+                    echoOutput.append(inputParts[i]);
+                    if (i < inputParts.length - 1) {
+                        echoOutput.append(" ");
+                    }
+                }
+                System.out.println(echoOutput.toString());
             } else if (command.equals("pwd")) {
                 System.out.println(System.getProperty("user.dir"));
             } else if (command.equals("cd")) {
-                String path = input.substring(3).trim();
+                if (inputParts.length < 2) {
+                    continue;
+                }
+                String path = inputParts[1];
                 File currentDir = new File(System.getProperty("user.dir"));
                 File targetDir;
 
@@ -48,7 +65,10 @@ public class Main {
                     System.out.println("cd: " + path + ": No such file or directory");
                 }
             } else if (command.equals("type")) {
-                String targetCommand = input.substring(5);
+                if (inputParts.length < 2) {
+                    continue;
+                }
+                String targetCommand = inputParts[1];
 
                 if (targetCommand.equals("echo")
                         || targetCommand.equals("exit")
@@ -94,11 +114,42 @@ public class Main {
                             .start();
                     process.waitFor();
                 } else {
-                    System.out.println(input + ": command not found");
+                    System.out.println(command + ": command not found");
                 }
             }
         }
 
         scanner.close();
+    }
+
+    private static List<String> parseArguments(String input) {
+        List<String> args = new ArrayList<>();
+        StringBuilder currentArg = new StringBuilder();
+        boolean inSingleQuotes = false;
+        boolean hasContent = false;
+
+        for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+
+            if (c == '\'') {
+                inSingleQuotes = !inSingleQuotes;
+                hasContent = true;
+            } else if (c == ' ' && !inSingleQuotes) {
+                if (hasContent || currentArg.length() > 0) {
+                    args.add(currentArg.toString());
+                    currentArg.setLength(0);
+                    hasContent = false;
+                }
+            } else {
+                currentArg.append(c);
+                hasContent = true;
+            }
+        }
+
+        if (hasContent || currentArg.length() > 0) {
+            args.add(currentArg.toString());
+        }
+
+        return args;
     }
 }
