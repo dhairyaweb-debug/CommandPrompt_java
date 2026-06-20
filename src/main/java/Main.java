21dbed5957fcd99af5b68fdec0fc412c28c9cbca
Dhairya <dhairyaweb@gmail.com>
2026-06-20 15:07:09 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,19 +11,30 @@ public class Main {
 
             if (input.equals("exit")) {
                 break;
-            }
-            if (input.startsWith("echo ")) {
+            } else if (input.startsWith("echo ")) {
                 System.out.println(input.substring(5));
             } else if (input.startsWith("type ")) {
                 String command = input.substring(5);
 
-                if (command.equals("echo") || command.equals("exit") || command.equals("type")) {
+                if (command.equals("echo")
+                        || command.equals("exit")
+                        || command.equals("type")) {
                     System.out.println(command + " is a shell builtin");
                 } else {
-                    String path = findInPath(command);
-                    if (path != null) {
-                        System.out.println(command + " is " + path);
-                    } else {
+                    String pathEnv = System.getenv("PATH");
+                    String[] directories = pathEnv.split(java.util.regex.Pattern.quote(File.pathSeparator));
+                    boolean found = false;
+
+                    for (String directory : directories) {
+                        File executableFile = new File(directory, command);
+                        if (executableFile.exists() && executableFile.canExecute()) {
+                            System.out.println(command + " is " + executableFile.getAbsolutePath());
+                            found = true;
+                            break;
+                        }
+                    }
+
+                    if (!found) {
                         System.out.println(command + ": not found");
                     }
                 }
@@ -31,17 +42,7 @@ public class Main {
                 System.out.println(input + ": command not found");
             }
         }
-    }
 
-    private static String findInPath(String command) {
-        String pathEnv = System.getenv("PATH");
-        if (pathEnv == null) return null;
-        for (String dir : pathEnv.split(File.pathSeparator)) {
-            File file = new File(dir, command);
-            if (file.isFile() && file.canExecute()) {
-                return file.getAbsolutePath();
-            }
-        }
-        return null;
+        scanner.close();
     }
 }
